0f8cc494456a03faec7ebd000f25ef2094eb97af
Lena Izmer <lenalesnaya1993@gmail.com>
2021-12-09 08:21:05 +0300

    Page and Controller is edited

--- ItechArt.Survey/ItechArt.Survey.WebApp/Controllers/HomeController.cs
+++ ItechArt.Survey/ItechArt.Survey.WebApp/Controllers/HomeController.cs
@@ -1,45 +1,26 @@
 ﻿using ItechArt.Survey.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+
+
 
 namespace ItechArt.Survey.WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        private static int Counter = 0;
+        [HttpGet]
         public IActionResult HomePage()
         {
-            return View();
+            var model = new CounterVm();
+            model.Counter = Counter;
+            return View(model);
         }
-
-        [HttpGet]
-        public IActionResult clickButtonFunction(int? clickCounter)
+        [HttpPost]
+        public IActionResult IncrementCounter(CounterVm model)
         {
-            ViewData["clickCounter"] = clickCounter == null ? 0 : clickCounter + 1;
-
-            return View("HomePage");
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            Counter++;
+            model.Counter = Counter;
+            return RedirectToAction("HomePage");
         }
     }
 }
