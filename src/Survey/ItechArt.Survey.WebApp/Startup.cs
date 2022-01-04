e88741e41e207801ca32f58c790614ac0d7b2a92
smyglifreind <heits_rs001@mail.ru>
2022-01-04 18:43:11 +0300

    feat: add Counter table

--- src/Survey/ItechArt.Survey.WebApp/Startup.cs
+++ src/Survey/ItechArt.Survey.WebApp/Startup.cs
@@ -4,6 +4,7 @@ using ItechArt.Survey.WebApp.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
