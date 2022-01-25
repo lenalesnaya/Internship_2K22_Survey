d302399a7a988b9cc544eb08c3fdaa1af31adf10
lenalesnaya <lenalesnaya1993@gmail.com>
2022-01-25 19:55:32 +0300

    Initial attempts to implement logging (install logging package Serilog; implement ILogger interface in ItechArt.Common; setup logger configuraton, so that log data will be saved in a file Survey.log in the application folder; modify output template to control formatting of a log row).

---
+++ src/Common/ItechArt.Common/Logger.cs
@@ -0,0 +1,16 @@
+﻿using Serilog;
+using Serilog.Events;
+
+namespace ItechArt.Common
+{
+    public class Logger : ILogger
+    {
+        public void Write(LogEvent logEvent)
+        {
+            using var log = new LoggerConfiguration().WriteTo.File(
+                "../../Survey/ItechArt.Survey.WebApp/bin/Debug/net6.0/logs/Survey.log",
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .CreateLogger();
+        }
+    }
+}
--- src/Survey/ItechArt.Survey.WebApp/Program.cs
+++ src/Survey/ItechArt.Survey.WebApp/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using Serilog;
 using ItechArt.Survey.Repositories;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -11,13 +12,39 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var host = CreateHostBuilder(args).Build();
-        MigrateDbContext<SurveyDbContext>(host.Services);
-        host.Run();
+        Log.Logger = new LoggerConfiguration().WriteTo.File(
+            "bin/Debug/net6.0/logs/Survey.log",
+            outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+            .CreateLogger();
+        Log.Information("Started!");
+
+        try
+        {
+            var host = CreateHostBuilder(args).Build();
+            MigrateDbContext<SurveyDbContext>(host.Services);
+            host.Run();
+
+            Log.Information("Stopped cleanly!");
+        }
+        catch (Exception exception)
+        {
+            Log.Fatal(exception, exception.Message);
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args)
         => Host.CreateDefaultBuilder(args)
+            .UseSerilog((context, services, configuration)
+            => configuration.ReadFrom.Configuration(context.Configuration)
+                 .ReadFrom.Services(services)
+                 .Enrich.FromLogContext()
+                 .WriteTo.File(
+                "bin/Debug/net6.0/logs/Survey.log",
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}"))
             .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
 
 
