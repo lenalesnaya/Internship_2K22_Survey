7d541f698a37bfc49fba42f45d295e7b4dd66670
smyglifreind <heits_rs001@mail.ru>
2022-02-10 00:26:23 +0300

    code style

--- src/Survey/ItechArt.Survey.Foundation/Authentication/Abstractions/IAuthenticateService.cs
+++ src/Survey/ItechArt.Survey.Foundation/Authentication/Abstractions/IAuthenticateService.cs
@@ -4,6 +4,6 @@ using ItechArt.Survey.DomainModel;
 namespace ItechArt.Survey.Foundation.Authentication.Abstractions;
 
 public interface IAuthenticateService
-{
+{ 
     Task<OperationResult<int, UserRegistrationStatus>> RegistrationAsync(User user, string password);
 }
