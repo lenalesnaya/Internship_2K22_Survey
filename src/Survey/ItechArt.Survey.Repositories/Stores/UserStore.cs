8c41679c41e7adfc7c66e8b7dac4e6609d4bde34
smyglifreind <heits_rs001@mail.ru>
2022-02-15 11:04:17 +0300

    add UserStore, but no implement

---
+++ src/Survey/ItechArt.Survey.Repositories/Stores/UserStore.cs
@@ -0,0 +1,65 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ItechArt.Survey.DomainModel;
+using Microsoft.AspNetCore.Identity;
+
+namespace ItechArt.Survey.Repositories.Stores;
+
+public class UserStore<TUser> : IUserStore<User>
+    where TUser: User
+{
+    public void Dispose()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<string> GetUserIdAsync(User user, CancellationToken cancellationToken)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<string> GetUserNameAsync(User user, CancellationToken cancellationToken)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task SetUserNameAsync(User user, string userName, CancellationToken cancellationToken)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<string> GetNormalizedUserNameAsync(User user, CancellationToken cancellationToken)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task SetNormalizedUserNameAsync(User user, string normalizedName, CancellationToken cancellationToken)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<IdentityResult> CreateAsync(User user, CancellationToken cancellationToken)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<IdentityResult> UpdateAsync(User user, CancellationToken cancellationToken)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<IdentityResult> DeleteAsync(User user, CancellationToken cancellationToken)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<User> FindByIdAsync(string userId, CancellationToken cancellationToken)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<User> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+    {
+        throw new System.NotImplementedException();
+    }
+}
