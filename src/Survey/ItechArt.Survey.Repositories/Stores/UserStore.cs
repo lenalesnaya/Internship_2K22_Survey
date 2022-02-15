b999638cce1ef9d3d0e15a0056031938a7abb24d
smyglifreind <heits_rs001@mail.ru>
2022-02-15 20:12:47 +0300

    add UserStore

--- src/Survey/ItechArt.Survey.Repositories/Stores/UserStore.cs
+++ src/Survey/ItechArt.Survey.Repositories/Stores/UserStore.cs
@@ -1,65 +1,128 @@
-﻿using System.Threading;
+﻿using System;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using ItechArt.Repositories.Abstractions;
 using ItechArt.Survey.DomainModel;
 using Microsoft.AspNetCore.Identity;
 
 namespace ItechArt.Survey.Repositories.Stores;
 
-public class UserStore<TUser> : IUserStore<User>
-    where TUser: User
+public class UserStore : IUserStore<User>
 {
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UserStore(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
     public void Dispose()
     {
-        throw new System.NotImplementedException();
+        _unitOfWork.Dispose();
+        GC.SuppressFinalize(this);
     }
 
-    public Task<string> GetUserIdAsync(User user, CancellationToken cancellationToken)
+    public async Task<string> GetUserIdAsync(User user, CancellationToken cancellationToken)
+        => (await _unitOfWork
+            .GetRepository<User>()
+            .GetWhereAsync(u => 
+                u.Id == user.Id))
+            .SingleOrDefault()
+            .ToString();
+
+    public async Task<string> GetUserNameAsync(User user, CancellationToken cancellationToken)
+        => (await _unitOfWork
+            .GetRepository<User>()
+            .GetWhereAsync(u => 
+                u.UserName == user.UserName))
+            .SingleOrDefault()
+            .UserName;
+
+    public async Task SetUserNameAsync(User user, string userName, CancellationToken cancellationToken)
     {
-        throw new System.NotImplementedException();
+        user = (await _unitOfWork
+            .GetRepository<User>()
+            .GetWhereAsync(u =>
+                u.Id == user.Id))
+            .SingleOrDefault();
+        user.UserName = userName;
     }
 
-    public Task<string> GetUserNameAsync(User user, CancellationToken cancellationToken)
+    public async Task<string> GetNormalizedUserNameAsync(User user, CancellationToken cancellationToken)
+        => (await _unitOfWork
+                .GetRepository<User>()
+                .GetWhereAsync(u => 
+                    u.NormalizedUserName == user.NormalizedUserName))
+            .SingleOrDefault()
+            .NormalizedUserName;
+
+    public async Task SetNormalizedUserNameAsync(User user, string normalizedName, CancellationToken cancellationToken)
     {
-        throw new System.NotImplementedException();
+        user = (await _unitOfWork
+                .GetRepository<User>()
+                .GetWhereAsync(u =>
+                    u.Id == user.Id))
+            .SingleOrDefault();
+        user.NormalizedEmail = normalizedName;
     }
 
-    public Task SetUserNameAsync(User user, string userName, CancellationToken cancellationToken)
+    public async Task<IdentityResult> CreateAsync(User user,  CancellationToken cancellationToken)
     {
-        throw new System.NotImplementedException();
+        var repository = _unitOfWork.GetRepository<User>();
+        repository.Add(user);
+        try
+        {
+            await _unitOfWork.SaveChangesAsync();
+        }
+        catch (Exception e)
+        {
+            return IdentityResult.Failed(new IdentityError{ Description = $"Exception: {e}"});
+        }
+        return IdentityResult.Success;
     }
 
-    public Task<string> GetNormalizedUserNameAsync(User user, CancellationToken cancellationToken)
+    public async Task<IdentityResult> UpdateAsync(User user,  CancellationToken cancellationToken)
     {
-        throw new System.NotImplementedException();
+        var repository = _unitOfWork.GetRepository<User>();
+        repository.Update(user);
+        try
+        {
+            await _unitOfWork.SaveChangesAsync();
+        }
+        catch (Exception e)
+        {
+            return IdentityResult.Failed(new IdentityError{ Description = $"Exception: {e}"});
+        }
+        return IdentityResult.Success;
     }
 
-    public Task SetNormalizedUserNameAsync(User user, string normalizedName, CancellationToken cancellationToken)
+    public async Task<IdentityResult> DeleteAsync(User user, CancellationToken cancellationToken)
     {
-        throw new System.NotImplementedException();
+        var repository = _unitOfWork.GetRepository<User>();
+        repository.Remove(user);
+        try
+        {
+            await _unitOfWork.SaveChangesAsync();
+        }
+        catch (Exception e)
+        {
+            return IdentityResult.Failed(new IdentityError{ Description = $"Exception: {e}"});
+        }
+        return IdentityResult.Success;
     }
 
-    public Task<IdentityResult> CreateAsync(User user, CancellationToken cancellationToken)
-    {
-        throw new System.NotImplementedException();
-    }
+    public async Task<User> FindByIdAsync(string userId, CancellationToken cancellationToken)
+        => (await _unitOfWork
+            .GetRepository<User>()
+            .GetWhereAsync(u => 
+                u.Id.ToString() == userId))
+            .SingleOrDefault();
 
-    public Task<IdentityResult> UpdateAsync(User user, CancellationToken cancellationToken)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public Task<IdentityResult> DeleteAsync(User user, CancellationToken cancellationToken)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public Task<User> FindByIdAsync(string userId, CancellationToken cancellationToken)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public Task<User> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
-    {
-        throw new System.NotImplementedException();
-    }
+    public async Task<User> FindByNameAsync(string userName, CancellationToken cancellationToken)
+        => (await _unitOfWork
+            .GetRepository<User>()
+            .GetWhereAsync(u =>
+                u.UserName.ToString() == userName))
+            .SingleOrDefault();
 }
