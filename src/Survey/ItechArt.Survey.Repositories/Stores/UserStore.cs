77181d362c5e827564b17b976fba752d0b903b24
smyglifreind <heits_rs001@mail.ru>
2022-02-15 21:20:45 +0300

    implement IUserEmailStore, IUserPasswordStore

--- src/Survey/ItechArt.Survey.Repositories/Stores/UserStore.cs
+++ src/Survey/ItechArt.Survey.Repositories/Stores/UserStore.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace ItechArt.Survey.Repositories.Stores;
 
-public class UserStore : IUserStore<User>
+public class UserStore : IUserEmailStore<User>, IUserPasswordStore<User>
 {
     private readonly IUnitOfWork _unitOfWork;
 
@@ -23,7 +23,7 @@ public class UserStore : IUserStore<User>
         GC.SuppressFinalize(this);
     }
 
-    public async Task<string> GetUserIdAsync(User user, CancellationToken cancellationToken)
+    public async Task<string> GetUserIdAsync(User user, CancellationToken cancellationToken = default)
         => (await _unitOfWork
             .GetRepository<User>()
             .GetWhereAsync(u => 
@@ -31,7 +31,7 @@ public class UserStore : IUserStore<User>
             .SingleOrDefault()
             .ToString();
 
-    public async Task<string> GetUserNameAsync(User user, CancellationToken cancellationToken)
+    public async Task<string> GetUserNameAsync(User user, CancellationToken cancellationToken = default)
         => (await _unitOfWork
             .GetRepository<User>()
             .GetWhereAsync(u => 
@@ -39,17 +39,13 @@ public class UserStore : IUserStore<User>
             .SingleOrDefault()
             .UserName;
 
-    public async Task SetUserNameAsync(User user, string userName, CancellationToken cancellationToken)
+    public async Task SetUserNameAsync(User user, string userName, CancellationToken cancellationToken = default)
     {
-        user = (await _unitOfWork
-            .GetRepository<User>()
-            .GetWhereAsync(u =>
-                u.Id == user.Id))
-            .SingleOrDefault();
         user.UserName = userName;
+        await UpdateAsync(user, cancellationToken);
     }
 
-    public async Task<string> GetNormalizedUserNameAsync(User user, CancellationToken cancellationToken)
+    public async Task<string> GetNormalizedUserNameAsync(User user, CancellationToken cancellationToken = default)
         => (await _unitOfWork
                 .GetRepository<User>()
                 .GetWhereAsync(u => 
@@ -57,20 +53,17 @@ public class UserStore : IUserStore<User>
             .SingleOrDefault()
             .NormalizedUserName;
 
-    public async Task SetNormalizedUserNameAsync(User user, string normalizedName, CancellationToken cancellationToken)
+    public async Task SetNormalizedUserNameAsync(User user, string normalizedName, CancellationToken cancellationToken = default)
     {
-        user = (await _unitOfWork
-                .GetRepository<User>()
-                .GetWhereAsync(u =>
-                    u.Id == user.Id))
-            .SingleOrDefault();
         user.NormalizedEmail = normalizedName;
+        await UpdateAsync(user, cancellationToken);
     }
 
-    public async Task<IdentityResult> CreateAsync(User user,  CancellationToken cancellationToken)
+    public async Task<IdentityResult> CreateAsync(User user,  CancellationToken cancellationToken = default)
     {
         var repository = _unitOfWork.GetRepository<User>();
         repository.Add(user);
+        
         try
         {
             await _unitOfWork.SaveChangesAsync();
@@ -79,13 +72,15 @@ public class UserStore : IUserStore<User>
         {
             return IdentityResult.Failed(new IdentityError{ Description = $"Exception: {e}"});
         }
+
         return IdentityResult.Success;
     }
 
-    public async Task<IdentityResult> UpdateAsync(User user,  CancellationToken cancellationToken)
+    public async Task<IdentityResult> UpdateAsync(User user,  CancellationToken cancellationToken = default)
     {
         var repository = _unitOfWork.GetRepository<User>();
         repository.Update(user);
+
         try
         {
             await _unitOfWork.SaveChangesAsync();
@@ -94,13 +89,15 @@ public class UserStore : IUserStore<User>
         {
             return IdentityResult.Failed(new IdentityError{ Description = $"Exception: {e}"});
         }
+
         return IdentityResult.Success;
     }
 
-    public async Task<IdentityResult> DeleteAsync(User user, CancellationToken cancellationToken)
+    public async Task<IdentityResult> DeleteAsync(User user, CancellationToken cancellationToken = default)
     {
         var repository = _unitOfWork.GetRepository<User>();
         repository.Remove(user);
+
         try
         {
             await _unitOfWork.SaveChangesAsync();
@@ -109,20 +106,74 @@ public class UserStore : IUserStore<User>
         {
             return IdentityResult.Failed(new IdentityError{ Description = $"Exception: {e}"});
         }
+
         return IdentityResult.Success;
     }
 
-    public async Task<User> FindByIdAsync(string userId, CancellationToken cancellationToken)
+    public async Task<User> FindByIdAsync(string userId, CancellationToken cancellationToken = default)
         => (await _unitOfWork
             .GetRepository<User>()
             .GetWhereAsync(u => 
                 u.Id.ToString() == userId))
             .SingleOrDefault();
 
-    public async Task<User> FindByNameAsync(string userName, CancellationToken cancellationToken)
+    public async Task<User> FindByNameAsync(string userName, CancellationToken cancellationToken = default)
         => (await _unitOfWork
             .GetRepository<User>()
             .GetWhereAsync(u =>
                 u.UserName.ToString() == userName))
             .SingleOrDefault();
+
+    public Task SetEmailAsync(User user, string email, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> GetEmailAsync(User user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> GetEmailConfirmedAsync(User user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SetEmailConfirmedAsync(User user, bool confirmed, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<User> FindByEmailAsync(string email, CancellationToken cancellationToken)
+        => (await _unitOfWork
+                .GetRepository<User>()
+                .GetWhereAsync(u =>
+                    u.Email == email))
+            .SingleOrDefault();
+
+    public Task<string> GetNormalizedEmailAsync(User user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SetNormalizedEmailAsync(User user, string normalizedEmail, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task SetPasswordHashAsync(User user, string passwordHash, CancellationToken cancellationToken)
+    {
+        user.PasswordHash = passwordHash;
+        await UpdateAsync(user, cancellationToken);
+    }
+
+    public Task<string> GetPasswordHashAsync(User user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> HasPasswordAsync(User user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
