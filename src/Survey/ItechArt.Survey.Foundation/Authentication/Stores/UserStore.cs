823860a6fedd57e69bd17f6cf5066e4a84c87539
smyglifreind <heits_rs001@mail.ru>
2022-02-17 00:33:54 +0300

    refactor UserStore

--- src/Survey/ItechArt.Survey.Foundation/Authentication/Stores/UserStore.cs
+++ src/Survey/ItechArt.Survey.Foundation/Authentication/Stores/UserStore.cs
@@ -21,35 +21,27 @@ public class UserStore : IUserEmailStore<User>, IUserPasswordStore<User>, IUserR
 
     public Task<string> GetUserIdAsync(User user, CancellationToken cancellationToken = default)
     {
-        return Task.FromResult(user.Id.ToString());
+        return user.Id.ToString();
     }
 
     public async Task<string> GetUserNameAsync(User user, CancellationToken cancellationToken = default)
-        => (await _unitOfWork
-            .GetRepository<User>()
-            .GetWhereAsync(u =>
-                u.UserName == user.UserName))
-            .SingleOrDefault()
-            .UserName;
+    {
+        return user.UserName;
+    }
 
     public async Task SetUserNameAsync(User user, string userName, CancellationToken cancellationToken = default)
     {
         user.UserName = userName;
-        await UpdateAsync(user, cancellationToken);
     }
 
     public async Task<string> GetNormalizedUserNameAsync(User user, CancellationToken cancellationToken = default)
-        => (await _unitOfWork
-                .GetRepository<User>()
-                .GetWhereAsync(u =>
-                    u.NormalizedUserName == user.NormalizedUserName))
-            .SingleOrDefault()
-            .NormalizedUserName;
+    {
+        return user.NormalizedUserName;
+    }
 
     public async Task SetNormalizedUserNameAsync(User user, string normalizedName, CancellationToken cancellationToken = default)
     {
         user.NormalizedEmail = normalizedName;
-        await UpdateAsync(user, cancellationToken);
     }
 
     public async Task<IdentityResult> CreateAsync(User user, CancellationToken cancellationToken = default)
@@ -83,87 +75,66 @@ public class UserStore : IUserEmailStore<User>, IUserPasswordStore<User>, IUserR
     }
 
     public async Task<User> FindByIdAsync(string userId, CancellationToken cancellationToken = default)
-        => (await _unitOfWork
-            .GetRepository<User>()
-            .GetWhereAsync(u =>
-                u.Id.ToString() == userId))
-            .SingleOrDefault();
+    {
+        var repository = _unitOfWork.GetRepository<User>();
+        var id = int.Parse(userId);
+        return await repository.SingleGetWhere(u => u.Id == id);
+    }
 
     public async Task<User> FindByNameAsync(string userName, CancellationToken cancellationToken = default)
-        => (await _unitOfWork
-            .GetRepository<User>()
-            .GetWhereAsync(u =>
-                u.UserName.ToString() == userName))
-            .SingleOrDefault();//пропарсить к int а потом 
+    {
+        var repository = _unitOfWork.GetRepository<User>();
+        return await repository.SingleGetWhere(u => u.UserName == userName);
+    }
 
     public async Task SetEmailAsync(User user, string email, CancellationToken cancellationToken)
     {
-        user.Email = email;
-        await UpdateAsync(user, cancellationToken);
+        user.Email = email;;
     }
 
     public async Task<string> GetEmailAsync(User user, CancellationToken cancellationToken)
-        => (await _unitOfWork
-            .GetRepository<User>()
-            .GetWhereAsync(u => 
-                u.Email == user.Email))
-            .SingleOrDefault()
-            .Email;
+    {
+        return user.Email;
+    }
 
     public async Task<bool> GetEmailConfirmedAsync(User user, CancellationToken cancellationToken)
-        => (await _unitOfWork
-                .GetRepository<User>()
-                .GetWhereAsync(u => 
-                    u.EmailConfirmed == user.EmailConfirmed))
-            .SingleOrDefault()
-            .EmailConfirmed;
+    {
+        return user.EmailConfirmed;
+    }
 
     public async Task SetEmailConfirmedAsync(User user, bool confirmed, CancellationToken cancellationToken)
     {
         user.EmailConfirmed = confirmed;
-        await UpdateAsync(user, cancellationToken);
     }
 
     public async Task<User> FindByEmailAsync(string email, CancellationToken cancellationToken)
-        => (await _unitOfWork
-                .GetRepository<User>()
-                .GetWhereAsync(u =>
-                    u.Email == email))
-            .SingleOrDefault();
+    {
+        var repository = _unitOfWork.GetRepository<User>();
+        return await repository.SingleGetWhere(u => u.Email == email);
+    }
 
     public async Task<string> GetNormalizedEmailAsync(User user, CancellationToken cancellationToken)
-        => (await _unitOfWork
-                .GetRepository<User>()
-                .GetWhereAsync(u => 
-                    u.NormalizedEmail == user.NormalizedEmail))
-            .SingleOrDefault()
-            .NormalizedEmail;
+    {
+        return user.NormalizedEmail;
+    }
 
     public async Task SetNormalizedEmailAsync(User user, string normalizedEmail, CancellationToken cancellationToken)
     {
         user.NormalizedEmail = normalizedEmail;
-        await UpdateAsync(user, cancellationToken);
     }
 
     public async Task SetPasswordHashAsync(User user, string passwordHash, CancellationToken cancellationToken)
     {
         user.PasswordHash = passwordHash;
-        await UpdateAsync(user, cancellationToken);
     }
 
     public async Task<string> GetPasswordHashAsync(User user, CancellationToken cancellationToken)
-        => (await _unitOfWork
-                .GetRepository<User>()
-                .GetWhereAsync(u => 
-                    u.PasswordHash == user.PasswordHash))
-            .SingleOrDefault()
-            .PasswordHash;
+    {
+        return user.PasswordHash;
+    }
 
     public async Task<bool> HasPasswordAsync(User user, CancellationToken cancellationToken)
     {
-        user = (await _unitOfWork
-            .GetRepository<User>()
-            .GetWhereAsync(u => u.Id == user.Id)).SingleOrDefault();
         return user.PasswordHash != null;
     }
 
@@ -251,6 +222,21 @@ public class UserStore : IUserEmailStore<User>, IUserPasswordStore<User>, IUserR
         }
 
         return users;
+    }
+
+
+    public async Task<IdentityResult> SaveChangesAsync()
+    {
+        try
+        {
+            await _unitOfWork.SaveChangesAsync();
+        }
+        catch (Exception exception)
+        {
+            throw new Exception(exception.Message);
+        }
+
+        return IdentityResult.Success;
     }
 
     public void Dispose()
