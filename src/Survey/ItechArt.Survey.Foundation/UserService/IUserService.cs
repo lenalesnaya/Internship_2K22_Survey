d56f210b5cec98597e264728dfdbac5856b96380
lenalesnaya <lenalesnaya1993@gmail.com>
2022-03-21 16:15:29 +0300

    Methods for finding users by email and name are added to the service; "post" is replaced by "get" where needed (and conversely); methods for real-time checking are optimized; "nameof" is entered, where it is possible.

--- src/Survey/ItechArt.Survey.Foundation/UserService/IUserService.cs
+++ src/Survey/ItechArt.Survey.Foundation/UserService/IUserService.cs
@@ -6,4 +6,8 @@ namespace ItechArt.Survey.Foundation.UserService;
 public interface IUserService
 {
     Task<User> GetUserByIdAsync(string userId);
+
+    Task<User> FindByUserNameAsync(string userName);
+
+    Task<User> FindByEmailAsync(string email);
 }
