ad47e6a5048da1fca98b2d7318c7601de73e8815
lenalesnaya <lenalesnaya1993@gmail.com>
2022-04-04 22:04:10 +0300

    RolesConstants is added; Layout is fixed.

---
+++ src/Survey/ItechArt.Survey.WebApp/Constants/RolesConstants.cs
@@ -0,0 +1,8 @@
+﻿using ItechArt.Survey.DomainModel;
+
+namespace ItechArt.Survey.WebApp.Constants;
+
+public class RolesConstants
+{
+    public const string AdministratorRoleName = Role.Administrator;
+}
